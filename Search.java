1b460b3d34ecb81f4a1eb690cb52dfc06345c8b0
Aleksander <aleksanderlucy1@gmail.com>
2019-07-22 14:30:31 -0400

    newer better search

---
+++ Search.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+// This search object has a value checker and an array list
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Search {
+
+    // Class variables
+    private ArrayList<Food> matching;
+
+
+    // Constructor
+    Search( ArrayList<Food> matching) {
+
+        this.matching = matching;
+
+        FoodRepository items = new FoodRepository();
+        Map<String, Food> foods = items.getFoods();
+
+        //something to set value from user input
+        String searchValue = "burger";
+
+        for(Map.Entry<String, Food> entry : foods.entrySet()) {
+            Food v = entry.getValue();
+            if(v.getTags().contains(searchValue)) {
+                matching.add(v);
+            }
+        }
+    }
+
+    // Getter methods
+    ArrayList<Food> getMatching() {
+        return matching;
+    }
+
+    // Setter methods
+    void setMatching(ArrayList<Food> matching) {
+        this.matching = matching;
+    }
+}
